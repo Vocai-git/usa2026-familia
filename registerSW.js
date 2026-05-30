3f548e43a457d68f94f324cd0e90c4385bd90ae4
Vocai-git <agus@vocai.es>
2026-05-30 11:32:39 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/usa2026-familia/sw.js', { scope: '/usa2026-familia/' })})}
